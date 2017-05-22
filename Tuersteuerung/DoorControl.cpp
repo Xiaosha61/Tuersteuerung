ef1f1f4975c30d2d621c6fc5c5244a5c445826fe
Xiaosha61 <xxssfamily61@gmail.com>
2017-05-22 21:35:08 +0200

    loop in run

--- Tuersteuerung/DoorControl.cpp
+++ Tuersteuerung/DoorControl.cpp
@@ -39,11 +39,24 @@ void DoorControl::setOutput(int ausgang)
 
 void DoorControl::run()
 {
-	int i = 500000;
+	//int i = 500000;
 	door_if.DebugString("Please wait 10 Seconds for auto exit, or press 'q'.");
+	/*
 	while( !door_if.quit_doorcontrol_flag && i-- ){
 		door_if.StartTimer(0.2);
-		cout << "what up" << endl;
+	}
+	*/
+	while(1){
+		door_if.DIO_Read(&inputSignal); // channels -> inputSignal
+		if(inputSignal & 32 == 32){
+			door_if.DIO_Write(4);
+		} else {
+			door_if.DIO_Write(5);
+		}
+
+		if(door_if.quit_doorcontrol_flag){
+			return;
+		}
 	}
 }
 
@@ -56,11 +69,8 @@ int main (int argc, char *argv[])
 	// ... insert your class initialisation and loop code here ...
 	// example start:
 	//int inputSignal;
-	int temp;
-	DoorControl control ;
-	
+	DoorControl control ;	
 	//control.setOutput(2);
-
 	control.run();
 
 	return 0;
