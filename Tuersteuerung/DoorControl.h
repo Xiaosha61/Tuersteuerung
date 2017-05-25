fc07bb3b7e31e67f032db3d0068a9c066aab7410
Xiaosha61 <xxssfamily61@gmail.com>
2017-05-25 21:19:59 +0200

    from vm

--- Tuersteuerung/DoorControl.h
+++ Tuersteuerung/DoorControl.h
@@ -18,18 +18,34 @@ public:
 	DoorControl();
 	~DoorControl();
 	
-	
-	int inputSignal;
-	//int S;
+	void Read_S1_S2_Signal();
+	void set_S1_S2_Signal();
+	int get_S1_S2_Signal();
+	void ModeSwitch();
+	void AutomatikMode();
+	void OpenDoorAndWait();
+	int TuerCloseBeurteilen();
+	void CloseDoorWithCondition();
+	int IfThereIsSth();
+	void HandMode();
+	void ReparaturMode();
+	void ProzessAusMode();
+	void CloseDoor();
+	void OpenDoor();
+	void Fehler();
 	void run();
-	void setInput(int *eingang);
-	void setOutput(int ausgang);
-	int getSignal();
+	
 private:
 	DoorInterface door_if;
-	
-	
+
+	int inputSignal = 0;
+	int mode;
+	int S1,S2,E1,E2,X1,X2,X3,LS1,LS2,BE,B;
+	//加上friend，来让外部的函数访问inputSignal
+
+
 };
 
 
 #endif // DOORCONTROL_HH
+
